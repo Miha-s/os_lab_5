0267b1b64dfd1dd85b7234c31ea1ae154c4dd868
MihaS <mishastatnik@gmail.com>
2024-04-21 20:14:16 +0300

    Additional refactoring

--- main.cpp
+++ main.cpp
@@ -16,7 +16,7 @@
 void send_mq(mqd_t mqd, const int data) {
     char* data_c;
     data_c = (char*)&data;
-    // std::cout << "send: " << data << std::endl;
+
     mq_send(mqd, data_c, sizeof(data), 0);
     if (errno != 0)
     {
@@ -43,23 +43,70 @@ bool read_mq(mqd_t mqd, int& data)
         std::cout << "errno " << errno << std::endl;
         return false;
     }
-    // std::cout << "ret: " << ret << std::endl;
 
     return true;
 }
 
 int f(int x) {
     std::this_thread::sleep_for(std::chrono::seconds(10));
-    return x * x;  // Приклад обчислення, ніколи не досягне
+    return x * x;
 }
 
 int g(int x) {
-    // std::this_thread::sleep_for(std::chrono::seconds(10));
-    return x;  // Приклад обчислення
+    return x; 
 }
 
-void prompt_decision() {
-    printf("1) continue execution\n2) stop\n3) continue execution, don't ask again\n");
+void print_info() {
+    printf("1) continue execution\n2) stop\n");
+}
+
+void run_main_part(mqd_t mqd, pid_t f_pid, pid_t g_pid) 
+{   
+    printf("PID of children processes: f: %d, g: %d\n", f_pid, g_pid);
+
+    int results[2] = {1, 1}, count = 0;
+    time_t start_time = time(NULL);
+
+    while (count < 2) {
+        if (read_mq(mqd, results[count])) {
+            printf("Got result form subprocess: %d\n", results[count]);
+            if (results[count] == 0) {
+                printf("One of results is equal to 0. End result: 0\n");
+                kill(f_pid, SIGKILL);
+                kill(g_pid, SIGKILL);
+                break;
+            }
+            count++;
+        }
+
+        if ( difftime(time(NULL), start_time) > 5) {
+            print_info();
+            int choice;
+            scanf("%d", &choice);
+            switch(choice) {
+                case 1: break;
+                case 2: 
+                    kill(f_pid, SIGKILL);
+                    kill(g_pid, SIGKILL);
+                    printf("Processes has been killed by user.\n");
+                    return;
+                default:
+                    printf("Ivalid value, continuing");
+            }
+            start_time = time(NULL);  // Скидання таймера
+        }
+    }
+
+    if (count == 2) {
+        int final_result = results[0] && results[1];
+        printf("Result f(x) && g(x) = %d\n", final_result);
+    }
+
+           
+    waitpid(f_pid, NULL, 0);
+    waitpid(g_pid, NULL, 0);
+    mq_close(mqd);
+            
 }
 
 int main() {
@@ -74,54 +121,13 @@ int main() {
     if(errno != 0) {
         std::cout << "Failed create queue " << errno << std::endl;
     }
-    // return 3;
-    pid_t pid_f = fork();
-    pid_t pid_g = pid_f ? fork() : 0;
 
-    if (pid_f != 0 && pid_g != 0) {
-      printf("PID of children processes: f: %d, g: %d\n", pid_f, pid_g);
-        int results[2] = {1, 1}, count = 0;
-        int continue_without_prompt = 0;
-        time_t start_time = time(NULL);
+    pid_t f_pid = fork();
+    pid_t g_pid = f_pid ? fork() : 0;
 
-        while (count < 2) {
-            if (read_mq(mqd, results[count])) {
-                printf("Got result form subprocess: %d\n", results[count]);
-                if (results[count] == 0) {
-                    printf("One of results is equal to 0. End result: 0\n");
-                    kill(pid_f, SIGKILL);
-                    kill(pid_g, SIGKILL);
-                    break;
-                }
-                count++;
-            }
-
-            if (!continue_without_prompt && difftime(time(NULL), start_time) > 5) {
-                prompt_decision();
-                int choice;
-                scanf("%d", &choice);
-                switch(choice) {
-                    case 1: break;
-                    case 2: 
-                        kill(pid_f, SIGKILL);
-                        kill(pid_g, SIGKILL);
-                        printf("Processes has been killed by user.\n");
-                        return 1;
-                    case 3: continue_without_prompt = 1; break;
-                }
-                start_time = time(NULL);  // Скидання таймера
-            }
-        }
-
-        if (count == 2) {
-            int final_result = results[0] && results[1];
-            printf("Result f(x) && g(x) = %d\n", final_result);
-        }
-        
-        waitpid(pid_f, NULL, 0);
-        waitpid(pid_g, NULL, 0);
-        mq_close(mqd);
-    } else if (pid_f == 0) {
+    if (f_pid != 0 && g_pid != 0) {
+      run_main_part(mqd, f_pid, g_pid);
+    } else if (f_pid == 0) {
         int result = f(x);
         mqd = mq_open(queue_name, O_RDWR | O_NONBLOCK);
         if(errno != 0) {
@@ -130,7 +136,7 @@ int main() {
         send_mq(mqd, result);
         mq_close(mqd);
         exit(EXIT_SUCCESS);
-    } else if (pid_g == 0) {
+    } else if (g_pid == 0) {
         int result = g(x);
         if(errno != 0) {
             std::cout << "failed open queue " << errno << std::endl;
